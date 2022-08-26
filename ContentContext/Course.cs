b21e827257da0486964f1ade87774cc235a39f65
fsanntiago <zerofabricio@hotmail.com>
2022-08-26 14:28:34 -0300

    feat: Add course

--- ContentContext/Course.cs
+++ ContentContext/Course.cs
@@ -1,6 +1,16 @@
-﻿namespace Balta.ContentContext;
+﻿using Balta.ContentContext.Enums;
+
+namespace Balta.ContentContext;
 
 public class Course : Content
 {
+    public Course()
+    {
+        Modules = new List<Module>();
+    }
+
     public string Tag { get; set; }
+    public IList<Module> Modules { get; set; }
+    public int DurationInMinutes { get; set; }
+    public EContentLevel Level { get; set; }
 }
---
+++ ContentContext/Enums/EContentLevel.cs
@@ -0,0 +1,9 @@
+﻿namespace Balta.ContentContext.Enums;
+
+public enum EContentLevel
+{
+    Beginner = 1,
+    Fundamental = 2,
+    intermediary = 3,
+    Advanced = 4
+}
